80fd7bbbe4808aa11a03a50fcb63adcf99b26c13
rechaile <rechaile@gmail.com>
2022-03-11 19:07:57 -0300

    creamos navbar

--- coder-app/src/components/navbar/NavBar.jsx
+++ coder-app/src/components/navbar/NavBar.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
 
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import Container from 'react-bootstrap/Container'
+import "./navBar.css" 
 function NavBar() {
-  return (
-    <div>NavBar</div>
+  return (    
+    <Navbar className='menu' expand="lg">
+    <Container>
+      <Navbar.Brand href="#home">
+        MercaditoFree
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Inicio</Nav.Link>
+          <Nav.Link href="#link">Ofertas</Nav.Link>
+          <Nav.Link href="#link">Full</Nav.Link>
+          <Nav.Link href="#link">Precios más bajos</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
   )
 }
 
